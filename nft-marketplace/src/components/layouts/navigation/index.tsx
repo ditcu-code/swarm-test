76d77bd3310a80c9a650601580501845781a763b
ditcu-code <work.adityac@gmail.com>
2025-11-12 21:47:20 +0700

    Update styles in App.css for improved layout and adjust navigation button dimensions in index.tsx

--- nft-marketplace/src/components/layouts/navigation/index.tsx
+++ nft-marketplace/src/components/layouts/navigation/index.tsx
@@ -4,7 +4,7 @@ import UserIcon from "../../../assets/icons/User.svg"
 
 export default function Navigation() {
   return (
-    <header className="w-full bg-(--color-background) border border-amber-50">
+    <header className="w-full bg-(--color-background)">
       <div className="mx-[50px] h-[100px] flex items-center justify-between">
         <Link to="/" className="flex items-center shrink-0 select-none">
           <img src={Logo} alt="NFT Marketplace" className="md:h-10 w-[243px] h-[32px]" />
@@ -23,10 +23,10 @@ export default function Navigation() {
 
           <Link
             to="/home"
-            className="font-semibold! px-[30px] inline-flex items-center gap-3 h-[60px] bg-(--color-primary) text-white! py-3 rounded-2xl shadow-sm hover:opacity-95 transition select-none"
+            className="font-semibold! px-[30px] inline-flex items-center gap-3 h-[60px] bg-(--color-primary) text-white! py-3 rounded-[20px] shadow-sm hover:opacity-95 transition select-none"
           >
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={UserIcon} alt="User" className="w-4 h-4" />
+            <img src={UserIcon} alt="User" className="w-5 h-5" />
             <span>Sign Up</span>
           </Link>
         </div>
